b9ff5d6e0221fb7a09b69734fb96eccfcc8fed21
SriHariJagan <sriharijagan04@gmail.com>
2026-01-23 11:57:07 +0530

    updated the footer

--- src/Pages/Footer/Footer.jsx
+++ src/Pages/Footer/Footer.jsx
@@ -4,37 +4,50 @@ import { Link } from "react-router-dom";
 import { Facebook, Instagram, Linkedin, Youtube } from "lucide-react";
 
 const navLinks = [
-  "Home",
-  "About Us",
-  "Products",
-  "Testimonials",
-  "Contact Us",
-  "Site Map",
+  { label: "Home", href: "/" },
+  { label: "About Us", href: "/about-us" },
+  { label: "Products", href: "/our-products" },
+  { label: "Marbles", href: "/marble" },
+  { label: "Imported Marbles", href: "/imported-marble" },
+  { label: "Granites", href: "/granites" },
+  { label: "Tiles", href: "/tiles" },
+  { label: "Sandstones", href: "/sandstone" },
+  { label: "Contact", href: "/contact" },
 ];
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.navLinks}>
-        {navLinks.map((link, i) => (
+        {navLinks.map((item, i) => (
           <span key={i} className={styles.navItem}>
-            {link}
+            <Link to={item.href} className={styles.navLink}>
+              {item.label}
+            </Link>
             {i < navLinks.length - 1 && <span className={styles.pipe}>|</span>}
           </span>
         ))}
       </div>
-
       <div className={styles.associatedSection}>
         <h4>Sister Concerns :</h4>
         <ul className={styles.associatedList}>
-          <li>PRIYA MARBLES <span className={styles.pipe}>|</span></li> 
-          <li>AKSHAY MARBLE SUPPLIERS <span className={styles.pipe}>|</span></li>
-          <li>SHRI KRISHNA STONEX <span className={styles.pipe}>|</span></li>
-          <li>SHUBHAM MARBLES <span className={styles.pipe}>|</span></li>
-          <li>MONU MARBLES <span className={styles.pipe}>|</span></li>
+          <li>
+            PRIYA MARBLES <span className={styles.pipe}>|</span>
+          </li>
+          <li>
+            AKSHAY MARBLE SUPPLIERS <span className={styles.pipe}>|</span>
+          </li>
+          <li>
+            SHRI KRISHNA STONEX <span className={styles.pipe}>|</span>
+          </li>
+          <li>
+            SHUBHAM MARBLES <span className={styles.pipe}>|</span>
+          </li>
+          <li>
+            MONU MARBLES <span className={styles.pipe}>|</span>
+          </li>
         </ul>
       </div>
-
       <div className={styles.mapSection}>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3566.704267410353!2d74.84740807488508!3d26.625924472357507!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x396bf1834ac2a471%3A0xbb07b44f710466d7!2sJK%20Grani%20Marmo!5e0!3m2!1sen!2sin!4v1752230576881!5m2!1sen!2sin"
@@ -46,7 +59,6 @@ const Footer = () => {
           title="JK Grani Marmo Location"
         ></iframe>
       </div>
-
       <div className={styles.bottomFooter}>
         <div className={styles.bottomBar}>
           <p>
@@ -66,8 +78,7 @@ const Footer = () => {
 
         <div className={styles.socialWrapper}>
           <div className={styles.socialIcons}>
-
-             <a
+            <a
               href="https://in.linkedin.com/in/jk-grani-marmo-38239926a"
               target="_blank"
               rel="noopener noreferrer"
